1204de1e460dd8a22f512662c57addd259b68eee
Chaya Deaver <chayadeaver@gmail.com>
2019-08-03 01:02:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,13 +33,15 @@ def read_from_hash(hash, key)
     hash[key] = value
     nil 
   end
+  hash
 end
 
 def update_counting_hash(hash, key)
   hash = new_hash
-  if hash[key] = value
+  if hash[key]
     hash[key] += 1
   else
     hash[key] = 1 
   end
+  hash
 end
